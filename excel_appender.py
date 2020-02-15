1340acea3c4dda5cde38ce1cc7506305bad91ab5
Alec McKay <alecmckay90@gmail.com>
2020-02-15 17:37:22 -0500

    fixed csv bug. Created directory sheet

--- excel_appender.py
+++ excel_appender.py
@@ -11,7 +11,11 @@ How to Use:
     with tables that you wish to append together. The 
     tables will be appended onto eachother in one 
     workbook named appendedBook.xlsx, in the same 
-    folder as the files you appended.
+    folder as the files you appended. A rightmost
+    column will be appended with the name of the 
+    file that the row was taken from. A worksheet 
+    "Directory" will also be created that lists the 
+    names of all files that were appended
 
 Requirements:
     1. Data to be appended must be in the first worksheet
@@ -25,7 +29,8 @@ Requirements:
 
 from os import listdir
 from tkinter import filedialog
-from tkinter import *
+from tkinter import Tk
+#from tkinter import *
 from tkinter import messagebox
 import pandas as pd
 import xlwings as xw
@@ -36,10 +41,15 @@ import sys
 def open_files():
     
     #initiate UI
-    root = Tk()
+    root = Tk()   
+    root.lift()
     
     root.filename = filedialog.askopenfilenames(initialdir="/", title="Select file")
     root.destroy()
+    
+    if root.filename == '':
+        sys.exit()
+    
     return root.filename
 
 #interface for displaying message
@@ -65,18 +75,24 @@ def append_books(filenames):
         
     directory = filenames[0][0: filenames[0].rfind('/')]
     
+    file_short_names = []
     
     for file in filenames:
         
         if(file[-3:len(file)] == 'xls' or file[-4:len(file)] == 'xlsx'):
-            appendBook = appendBook.append(pd.read_excel(file,sheet_name=0, index=False, index_label=False))
+            df = pd.read_excel(file,sheet_name=0, index=False, index_label=False)
             
         elif(file[-3:len(file)] == 'csv'):
-            appendBook = appendBook.append(pd.read_csv(file))
+            df = pd.read_csv(file)
             
         else:
             display_message("Error", "only select files of the type xlsx, xls or csv.")
             sys.exit("Wrong type of file appended")
+        
+        file_short_name = file[file.rfind('/') + 1:]
+        file_short_names.append(file_short_name)
+        df['source_name'] = [file_short_name] * len(df.index)
+        appendBook = appendBook.append(df)
         
     try:
         #write dataframe into file
@@ -86,21 +102,20 @@ def append_books(filenames):
         display_message("Error", "Cannot have workbook called appendedBook.xlsx open while running macro. Please close and try again")
         sys.exit("appendedBook.xlsx was open")
         
-    #open aggregated file
+    #open aggregated file and bring to focus
     appendedBook = xw.Book(directory + '\\appendedBook.xlsx')
     
-    #append file names onto string to print
-    printFiles = ''
+    directory = appendedBook.sheets.add("directory")
     
-    for file in filenames:
-        printFiles = printFiles + file[file.rfind('/') + 1:] + ', ' 
+    i = 1
     
-    printFiles = printFiles[:len(printFiles)-2]
-    
-    #display completion message
-    display_message("Macro Completed!","The following workbooks were aggregated: \n\n" + printFiles + "\n\n" + "in " + appendedBook.fullname)
+    for name in file_short_names:
+        directory.range('A' + str(i)).value = name
+        i += 1
    
-    #method to run script
+    appendedBook.sheets[1].activate()
+    xw.apps.active.activate(steal_focus=True)
+   
 def main():
     append_books(open_files())
     
