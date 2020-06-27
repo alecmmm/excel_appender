00f97ac9b31baca9d110286a9ca710d5e0683907
Alec McKay <alecmckay90@gmail.com>
2020-06-27 00:03:37 -0400

    duplicate name fixed
    program no longer exits if workbook name already taken. added create_file function

--- excel_appender.py
+++ excel_appender.py
@@ -4,33 +4,10 @@ Spyder Editor
 
 Written by Alec McKay, September, 2019
 
-Script for combining tables in Excel or csv workbooks.
-
-How to Use:
-    Run program. When prompted, select multiple files 
-    with tables that you wish to append together. The 
-    tables will be appended onto eachother in one 
-    workbook named appendedBook.xlsx, in the same 
-    folder as the files you appended. A rightmost
-    column will be appended with the name of the 
-    file that the row was taken from. A worksheet 
-    "Directory" will also be created that lists the 
-    names of all files that were appended
-
-Requirements:
-    1. Data to be appended must be in the first worksheet
-    2. All data must have the same headers
-    3. All workbooks must be in the same folder.
-    4. Workbook named appendedBook.xlsx must not be open
-    5. If there is a workbook names appendedBook.xlsx in 
-       the folder you're running it, it will be 
-       overridden
 """
 
-from os import listdir
 from tkinter import filedialog
 from tkinter import Tk
-#from tkinter import *
 from tkinter import messagebox
 import pandas as pd
 import xlwings as xw
@@ -39,11 +16,9 @@ import sys
 
 #interface for opening files
 def open_files():
-    
     #initiate UI
     root = Tk()   
     root.lift()
-    
     root.filename = filedialog.askopenfilenames(initialdir="/", title="Select file")
     root.destroy()
     
@@ -54,7 +29,6 @@ def open_files():
 
 #interface for displaying message
 def display_message(messageTitle, message):
-    
     #initiate UI
     root = Tk()
     
@@ -65,16 +39,30 @@ def display_message(messageTitle, message):
     messagebox.showinfo(messageTitle, message)
     root.destroy()
 
+#creates a file, appending an integer to its name if a file with that name
+#is already open
+def create_file(directory, file_name, df):
+    i = 1
+    
+    output_name = file_name
+    
+    while True:
+        
+        try:
+            df.to_excel(directory + output_name + ".xlsx")
+            return output_name
+            
+        except PermissionError:
+            i+= 1
+            output_name = file_name + str(i)
+
 #appends files together
 def append_books(filenames):
-       
     #loop through all files in directory. If file is of .xlsx type, doesn't begin with '~' 
-    #and isn't 'appendedBook.xlsx', then transform into a dataframe and append onto the 
+    #and isn't 'appended_book.xlsx', then transform into a dataframe and append onto the 
     #empty dataframe that was created.
-    appendBook = pd.DataFrame()
-        
+    appendBook = pd.DataFrame()  
     directory = filenames[0][0: filenames[0].rfind('/')]
-    
     file_short_names = []
     
     for file in filenames:
@@ -93,20 +81,12 @@ def append_books(filenames):
         file_short_names.append(file_short_name)
         df['source_name'] = [file_short_name] * len(df.index)
         appendBook = appendBook.append(df)
-        
-    try:
-        #write dataframe into file
-        appendBook.to_excel(directory + '\\appendedBook.xlsx')
-
-    except PermissionError:
-        display_message("Error", "Cannot have workbook called appendedBook.xlsx open while running macro. Please close and try again")
-        sys.exit("appendedBook.xlsx was open")
-        
+    
+    output_name = create_file(directory, "\\appended_book", appendBook)
+    
     #open aggregated file and bring to focus
-    appendedBook = xw.Book(directory + '\\appendedBook.xlsx')
-    
+    appendedBook = xw.Book(directory + output_name + '.xlsx')
     directory = appendedBook.sheets.add("directory")
-    
     i = 1
     
     for name in file_short_names:
